ce48ed249bd1909f76dc0620a9133f63fb48e932
Trevor Caira <toc3@cornell.edu>
2006-12-19 17:57:19 +0000

    Documented sam_main. Moved sam_exit_code to sam.h.

--- include/sam.h
+++ include/sam.h
@@ -26,14 +26,17 @@
  * SOFTWARE.
  *
  * $Log$
+ * Revision 1.6  2006/12/19 17:57:19  trevor
+ * Documented sam_main. Moved sam_exit_code to sam.h.
+ *
  * Revision 1.5  2006/12/19 10:41:26  anyoneeb
  * Last splint warnings fix for now.
  *
  * Revision 1.4  2006/12/12 23:31:35  trevor
- * Added the $Id$ and $Log$
- * Added the $Id$ and Revision 1.5  2006/12/19 10:41:26  anyoneeb
- * Added the $Id$ and Last splint warnings fix for now.
- * Added the $Id$ and tags and copyright notice where they were missing.
+ * Added the Id and Log tags and copyright notice where they were missing.
+ *
+ */
+
 #ifndef SAM_H
 #define SAM_H
 
@@ -47,6 +50,15 @@ typedef enum {
 /** Exit codes for main() in case of error. */
 typedef enum {
     SAM_EMPTY_STACK = -1,   /**< The stack was empty after program
+			     *	 execution. */
+    SAM_PARSE_ERROR = -2,   /**< There was a problem parsing input. */
+    SAM_USAGE	    = -3    /**< Usage was printed, there was a problem
+			     *	 parsing the command line args. */
+} sam_exit_code;
+
+typedef int (*sam_read_int_func)(/*@out@*/ int *n);
+typedef int (*sam_read_float_func)(/*@out@*/ double *f);
+typedef int (*sam_read_char_func)(/*@out@*/ char *c);
 typedef int (*sam_read_str_func)(/*@out@*/ char **s);
 typedef int (*sam_write_int_func)(int n);
 typedef int (*sam_write_float_func)(double f);
@@ -57,7 +69,7 @@ typedef int (*sam_write_str_func)(char *s);
  *  Input/output callbacks. Used by various i/o opcodes.
  */
 typedef struct {
- *  Input/output callbacks. Used by various io opcodes.
+    /*@null@*/ /*@dependent@*/ sam_read_int_func    read_int_func;
     /*@null@*/ /*@dependent@*/ sam_read_float_func  read_float_func;
     /*@null@*/ /*@dependent@*/ sam_read_char_func   read_char_func;
     /*@null@*/ /*@dependent@*/ sam_read_str_func    read_str_func;
@@ -72,6 +84,21 @@ typedef struct {
  *
  *  @param options The bitwise OR of the flags controlling libsam's
  *		   behavior.
+ *  @param file	   The file containing the sam source to interpret, or
+ *		   NULL if standard input should be read.
+ *  @param io_funcs The set of input/output functions which should be
+ *		    called when that particular form of input or output
+ *		    is requested by the sam program. Any of the members
+ *		    of the structure may be NULL if the default for that
+ *		    function should be used, or the entire structure may
+ *		    be NULL if all default functions should be used.
+ *
+ *  @return The return value of the sam value, or one of the special
+ *	    #sam_exit_code in case a return value could not be extracted
+ *	    from the source due to error.
+ */
+extern int sam_main(sam_options options,
+		    /*@null@*/ const char   *file,
 		    /*@null@*/ sam_io_funcs *io_funcs);
 
-		    /*@null@*/ const char  *file,
+#endif /* SAM_H */
