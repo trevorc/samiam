cd279b46f5fb46910a6cc687f6c0a28f22ff8eed
Trevor Caira <toc3@cornell.edu>
2006-12-25 00:18:18 +0000

    Allow DEFS to be specified.

--- rul.mk
+++ rul.mk
@@ -2,12 +2,13 @@
 
 CC=gcc
 RM=rm -f
-LDFLAGS=-lm
 WFLAGS=-ansi -pedantic -Wall -W -Wmissing-prototypes -Wmissing-declarations -Wstrict-prototypes -Wpointer-arith -Wunreachable-code -Wnested-externs -Wdeclaration-after-statement
 OFLAGS=-pipe -g -ggdb
 #OFLAGS=-pipe -O3 -msse3 -march=pentium-m
 INCLUDES=-I../include
-CFLAGS=$(WFLAGS) $(OFLAGS) $(INCLUDES)
+DEFS=
+CFLAGS=$(WFLAGS) $(OFLAGS) $(INCLUDES) $(DEFS)
+LDFLAGS=-lm
 
 VERSION=.1.0.0
 MAJOR=.1
