17b1f54b28cf021b858cda98737718bbcc6460e3
Trevor Caira <toc3@cornell.edu>
2007-02-06 23:20:03 +0000

    Tester commit fixes.

---
+++ tests/tester.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2.5
+
+import os
+import sqlite3 as sqlite
+
+class Tester:
+    def __init__(self, cmd, db, path=None, libpath=None, infcmd=None):
+	import os
+
+	self._cmd = cmd
+	self._db = self._read_db(db)
+
+	if path:
+	    os.putenv('PATH', path)
+	if libpath:
+	    os.putenv('LD_LIBRARY_PATH', libpath)
+	if infcmd:
+	    self._inf = os.system(infcmd)
+
+    def _read_db(self, db):
+	try:
+	    conn = sqlite.connect(db)
+	    cursor = conn.cursor()
+	    cursor.execute('select * from tests')
+	    return cursor.fetchall()
+	finally:
+	    conn.close()
+
+    def testall(self):
+	failed = []
+
+	# parallelize me
+	for test, status, input, output in self._db():
+	    actual_status, actual_output = self._run(input)
+
+if __name__ == '__main__':
+    tester = Tester(cmd='samiam', db='tests.db',
+		    path='../build/samiam', libpath='../build/libsam',
+		    infcmd='./inf')
