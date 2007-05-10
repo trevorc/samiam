6d11db825be535aa25f0178498816be14e6310a5
Trevor Caira <toc3@cornell.edu>
2007-05-10 20:22:32 +0000

    Added console script.

--- src/python/common.sh
+++ src/python/common.sh
@@ -23,9 +23,6 @@ sam_so_err() {
     exit 1
 }
 
-SAM_SO=`find build -name sam.so 2>/dev/null` || sam_so_err
-
-export LD_LIBRARY_PATH=../../build/libsam
-export PYTHONPATH=`dirname $SAM_SO`
+. env.sh
 
 python_args="$pyfile $samfile"
---
+++ src/python/env.sh
@@ -0,0 +1,4 @@
+SAM_SO=`find build -name sam.so 2>/dev/null` || sam_so_err
+
+export LD_LIBRARY_PATH=../../build/libsam
+export PYTHONPATH=`dirname $SAM_SO`
