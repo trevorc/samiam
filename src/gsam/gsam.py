b3cbd7ee148a3121f08afbd67160485dd4fd7731
Daniel Perelman <dap56@cornell.edu>
2007-05-11 05:02:48 +0000

    Naming in glade. Some gsam work.

--- src/gsam/gsam.py
+++ src/gsam/gsam.py
@@ -2,6 +2,7 @@
 
 import pygtk
 import gtk, gtk.glade
+import sam
 
 #import libsam
 
@@ -10,16 +11,47 @@ class GSam:
 	self._xml = gtk.glade.XML('gsam.glade')
 	self._xml.signal_autoconnect(self)
 
-	self._open_dialog = self._xml.get_widget('filechooser')
+	self._module_combobox = self._xml.get_widget('module_combobox')
+	self._code_view = self._xml.get_widget('code_view')
+	self._stack_view = self._xml.get_widget('stack_view')
+	self._heap_view = self._xml.get_widget('heap_view')
 	self._file = None
 
     def _filechooser_factory(self, title, p):
-	pass
+	dialog = gtk.FileChooserDialog(title=title, \
+		parent=self._xml.get_widget('main_window'),\
+		buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,\
+		    gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+	dialog.set_default_response(gtk.RESPONSE_OK)
+
+	filter = gtk.FileFilter()
+	filter.set_name("All files")
+	filter.add_pattern("*")
+	dialog.add_filter(filter)
+
+	filter = gtk.FileFilter()
+	filter.set_name("SaM files")
+	filter.add_pattern("*.sam")
+	dialog.add_filter(filter)
+
+	return dialog
 
     def on_open_activate(self, p):
-	if self._file:
-	    self._file.close()
-	filechooser = self._filechooser_factory(p)
+	filechooser = self._filechooser_factory("Select a SaM file", p)
+	response = filechooser.run()
+	if response == gtk.RESPONSE_OK:
+	    if self._file:
+		self._file.close()
+	    init_program_display(filechooser.get_filename())
+	filechooser.destroy()
+
+    def update_code_display(self)
+	pass
+#	TODO self._code_view
+
+    def init_program_display(self, filename)
+	self._file = filename
+	self._prog = sam.Program(filename)
 
     def gtk_main_quit(*self):
 	gtk.main_quit()
