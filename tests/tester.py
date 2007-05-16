d171046f5bc6018c713e617be6630d17b872515c
Trevor Caira <toc3@cornell.edu>
2007-05-16 04:46:39 +0000

    Stubbed some more.

--- tests/tester.py
+++ tests/tester.py
@@ -26,14 +26,18 @@ class Tester:
 	finally:
 	    conn.close()
 
+    def _run(self, test, input):
+	return None, None
+
     def testall(self):
 	failed = []
 
 	# parallelize me
-	for test, status, input, output in self._db():
-	    actual_status, actual_output = self._run(input)
+	for test, status, input, output in self._db:
+	    actual_status, actual_output = self._run(test, input)
 
 if __name__ == '__main__':
     tester = Tester(cmd='samiam', db='tests.db',
 		    path='../build/samiam', libpath='../build/libsam',
 		    infcmd='./inf')
+    tester.testall()
