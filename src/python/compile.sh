06b77381a2098e7d660f03b4defb49f7b070fc2d
Trevor Caira <toc3@cornell.edu>
2007-06-02 18:22:46 +0000

    Added extra checks.

--- src/python/compile.sh
+++ src/python/compile.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+if [ ! -d ../../build/python ]; then
+    mkdir -p ../../build/python
+fi
+
+if [ ! -f ../../build/libsam/libsam.so* ]; then
+    scons -C ../..
+fi
+
 CFLAGS='-std=c99 -Wall -W -g -ggdb' python setup.py build
